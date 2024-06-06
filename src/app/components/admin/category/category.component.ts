090aad13b534dd6d85f1364c760377f3439b72bd
TranvandatWhiteX <tranvandatdh012@gmail.com>
2024-06-06 07:25:38 +0700

    update: admin

--- src/app/components/admin/category/category.component.ts
+++ src/app/components/admin/category/category.component.ts
@@ -3,6 +3,9 @@ import { CategoryService } from '../../../services/category.service';
 import { ApiResponse } from '../../../responses/api.response';
 import { HttpErrorResponse } from '@angular/common/http';
 import { Category } from '../../../models/category';
+import { StorageResponse } from '../../../responses/storage.response';
+import { ArticleService } from '../../../services/article.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-category',
@@ -11,11 +14,31 @@ import { Category } from '../../../models/category';
 })
 export class CategoryComponent implements OnInit {
   categories: Category[];
-  constructor(private categoryService: CategoryService) {
+  articleCategories?: { id: string, name: string }[];
+  isProducts: boolean = true;
+  categoryForm: FormGroup;
+
+  constructor(private categoryService: CategoryService, private articleService: ArticleService, private fb: FormBuilder) {
+    this.categoryForm = this.fb.group({
+      category: ['', Validators.required]
+    })
     this.categories = [];
   }
   ngOnInit(): void {
     this.getCategories();
+  }
+  isArticleCategories() {
+    this.isProducts = false;
+  }
+  createCategory() {
+    if (this.categoryForm.valid) {
+      if (this.isProducts) {
+        console.log(this.categoryForm.value.category, "Products");
+      } else {
+        console.log(this.categoryForm.value.category, "Articles");
+        this.isProducts = true;
+      }
+    }
   }
   getCategories() {
     this.categoryService.getAllCategories().subscribe({
@@ -24,6 +47,17 @@ export class CategoryComponent implements OnInit {
         const { categories } = apiResponse.data;
         this.categories = categories;
         // console.log(this.categories);
+      },
+      error: (error: HttpErrorResponse) => {
+        console.error(error?.error?.message ?? '');
+      }
+    })
+  }
+  getAllArticleCategories() {
+    this.articleService.getAllArticleCategories().subscribe({
+      next: (apiResponse: ApiResponse<StorageResponse<{ id: string, name: string }[]>>) => {
+        this.articleCategories = apiResponse.data.articleCategories;
+        console.log(apiResponse.data);
       },
       error: (error: HttpErrorResponse) => {
         console.error(error?.error?.message ?? '');
